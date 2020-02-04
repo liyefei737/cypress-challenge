6a5bd0d20a4269e82e4c991f3f28bdc2910d4234
Chris Yoon <chrisyoon@users.noreply.github.com>
2020-02-04 18:44:04 -0500

    adding max number boundaries

--- cypress/integration/cyoontest.js
+++ cypress/integration/cyoontest.js
@@ -13,15 +13,21 @@ describe('Enter a number to get the median of primes:',() => {
         cy.compareResult("1","[,]")
         cy.compareResult("2","[,]")
         cy.compareResult("3","[2]")
-        cy.compareResult("0","[,]")        
+        cy.compareResult('10000000','[4751053]') 
+        cy.compareResult("0","[,]") 
+            
     })
 
     it('Functional acceptance testing - negative testings', () => {
         cy.compareResult("-1","[,]")
+
     })
       
     it('Find out error handling issue - sending string should disable the button', () => { 
+       
         cy.sendStringResult("helloworld")
+        cy.sendStringResult('10000001') 
+        // found out that the entry is bigger than the max number
     })
 
     it('testing null value submit', () => {
